7b96263e815b58d5222d16c7a31d69c4fb5a5036
Jeff L <jjl01332@gmail.com>
2024-01-24 15:35:02 -0500

    new branch, readme and logger

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,10 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+function logMsg(){
+    console.log(this.errMsg);
+}
+
+const randomObj = {
+    errMsg: "This is an error!"
+}
+
+logMsg.call(randomObj);
