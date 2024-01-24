611c0e5b04e7a6dfeb632f220e6e55bbbf17a7ae
Jeff L <jjl01332@gmail.com>
2024-01-24 16:16:09 -0500

    arrows and closures

--- quiz/scripts/closure.js
+++ quiz/scripts/closure.js
@@ -1,13 +1,16 @@
 function setColor(set) {
-        let changeColor = set;
-        if(changeColor) {
+    let changeColor = set;
+    return function setColor() {
+        if(changeColor){
             let userColor = document.getElementById('color').value;
             document.getElementById('myPara').style.color = userColor;
         }
-        
+    }
 }
 
 window.onload = function() {
     let toggle = true;
     document.getElementById('btn').onclick = setColor(toggle);
+    toggle = false;
+    console.log(toggle);
 }
